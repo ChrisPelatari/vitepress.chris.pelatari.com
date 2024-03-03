7a1a6301bc8ec40f3b9e87a7490fe64d1a299fe7
Chris Pelatari <chris@bluefenix.net>
2024-03-02 21:54:45 -0600

    Update post.vue

--- blog/.vitepress/theme/components/post.vue
+++ blog/.vitepress/theme/components/post.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="VPPage" style="margin:0.3rem; margin-top: 1.618rem;">
+  <div class="VPPage" style="margin:0.3rem;">
     <h3 class="text-h3">{{ frontmatter.title }}</h3>
     <p class="text-overline">{{ new Date(page.filePath.split('/').pop().slice(0, 10)).toDateString() }}</p>
     <p class="text-caption" v-if="frontmatter.permalink">{{ frontmatter.permalink }}</p>
