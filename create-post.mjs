849266917142c49e65b13abc3fe4c1a37dededfd
Chris Pelatari <chris@bluefenix.net>
2024-10-04 01:08:30 -0500

    Update create-post.mjs

--- create-post.mjs
+++ create-post.mjs
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import { argv } from 'process'
+import { exec } from 'child_process'
 
 // Get the title from the command line arguments
 const title = argv[2]
@@ -35,9 +36,20 @@ const fileContents =
   excerpt:
 ---
 
+# ${title}
 `
+// change to the blog directory
+process.chdir('blog')
+
+// change to the posts directory
+process.chdir('posts')
 
 // Create the file
 fs.writeFileSync(fileName, fileContents, 'utf-8')
 
 console.log(`Created file: ${fileName}`)
+
+console.log('Opening the file in the default editor...')
+
+// open the file in the default editor
+exec(`code ${fileName}`);
